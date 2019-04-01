112c9a261e7072ed822a66c169d59a2707cfde5b
Ahmed Osama <ahmed.osama.abbas@gmail.com>
2019-04-01 19:58:16 +0200

    Update CircularAnimation.swift

--- CircularAnimation.swift
+++ CircularAnimation.swift
@@ -33,38 +33,25 @@ class CircularAnimation {
     /// * Negative value will result in a counter-clockwise animation.
     var degrees: Float = 0
     
+    /// Indicates whether the final shape is meant to be a full circle or not.
+    /// * Generally, mark it as `true` if both first and last views overlap on each other.
+    var fullCircle = false
+    
     /// Duration of animation for each view in **seconds**.
     var duration: CFTimeInterval = 0
     
     /// The delay between animating views in **seconds**.
     var delay: CFTimeInterval = 0
     
-    /// Indicates whether the final shape is meant to be a full circle or not.
-    /// * Generally, mark it as `true` if both first and last views overlap on each other.
-    var fullCircle = false
-    
     /// A timing function defining the pacing of the animation.
     var timingFunction = TimingFunction.easeOutQuint()
     
-    func animate() {
-        let sourcePoint = getPointOnCircle(angle: sourceAngle)
-        for i in 0 ..< views.count {
-            views[i].center = sourcePoint
-            animate(view: views[i], index: i)
-        }
-    }
+    /// The order which views will be animated in.
+    var animatingOrder: AnimatingOrder = .default
     
-    fileprivate func animate(view: UIView, index: Int) {
+    func animate(mode: Mode, options: Options? = nil) {
         
-        guard sourceAngle >= 0 && sourceAngle <= 360 else {
-            return
-        }
-        
-        guard startAngle >= 0 && startAngle <= 360 else {
-            return
-        }
-        
-        guard degrees >= -360 && degrees <= 360 else {
+        guard validValues() else {
             return
         }
         
@@ -72,42 +59,75 @@ class CircularAnimation {
         let viewsDegreesDiff = abs(degrees) / Float(views.count - (fullCircle ? 0 : 1))
         let clockwise = degrees >= 0
         let sourceStartDegreesDiff = degreesDifference(a: sourceAngle, b: startAngle, clockwise: clockwise)
-        let totalDegrees = sourceStartDegreesDiff + (viewsDegreesDiff * Float(index))
-        let totalValues = totalDegrees / 0.1
-        let strid = stride(from: 0, to: totalDegrees, by: 0.1)
         
-        var values = [CGPoint]()
-        var keyTimes = [NSNumber]()
-        var valueCount: Float = 0
-        var angle = sourceAngle
-        for _ in strid {
-            if angle < 0 {
-                angle = 359.9
+        for index in 0 ..< views.count {
+            
+            let view = views[index]
+            
+            let totalDegrees = sourceStartDegreesDiff + (viewsDegreesDiff * Float(index))
+            let totalValues = totalDegrees / 0.1
+            let strid = stride(from: 0, to: totalDegrees, by: 0.1)
+            
+            var values = [CGPoint]()
+            var keyTimes = [NSNumber]()
+            var valueCount: Float = 0
+            var angle = sourceAngle
+            for _ in strid {
+                if angle < 0 {
+                    angle = 359.9
+                }
+                if angle >= 360 {
+                    angle = 0
+                }
+                values.append(getPointOnCircle(angle: angle))
+                keyTimes.append(NSNumber(value: valueCount / totalValues))
+                valueCount += 1
+                angle += 0.1 * one
             }
-            if angle >= 360 {
-                angle = 0
+            
+            if mode == .exit {
+                values.reverse()
             }
-            values.append(getPointOnCircle(angle: angle))
-            keyTimes.append(NSNumber(value: valueCount / totalValues))
-            valueCount += 1
-            angle += 0.1 * one
+            
+            let sourcePoint = getPointOnCircle(angle: sourceAngle)
+            DispatchQueue.main.async {
+                view.center = values.first ?? sourcePoint
+            }
+            
+            let keyPath = "position"
+            let animation = CAKeyframeAnimation()
+            animation.keyPath = keyPath
+            animation.values = values
+            animation.keyTimes = keyTimes
+            animation.duration = options?.duration ?? duration
+            animation.timingFunction = options?.timingFunction ?? timingFunction
+            
+            let animOrder = options?.animatingOrder ?? animatingOrder
+            let delayCount = Double(animOrder == .default ? index : views.count - index)
+            let delayValue = options?.delay ?? delay
+            DispatchQueue.main.asyncAfter(deadline: .now() + (delayValue * delayCount)) {
+                view.layer.position = values.last ?? view.layer.position
+                // Change model layer, as animation chnges presentation layer only
+                view.layer.add(animation, forKey: keyPath)
+                // Use the name of the animated property as key to override the implicit animation
+                // https://oleb.net/blog/2012/11/prevent-caanimation-snap-back/
+            }
+            
         }
         
-        let keyPath = "position"
-        let animation = CAKeyframeAnimation()
-        animation.keyPath = keyPath
-        animation.values = values
-        animation.keyTimes = keyTimes
-        animation.duration = duration
-        animation.timingFunction = timingFunction
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + (delay * Double(views.count - index))) {
-            view.layer.position = values.last ?? view.layer.position
-            // Change model layer, as animation chnges presentation layer only
-            view.layer.add(animation, forKey: keyPath)
-            // Use the name of the animated property as key to override the implicit animation
-            // https://oleb.net/blog/2012/11/prevent-caanimation-snap-back/
+    }
+    
+    fileprivate func validValues() -> Bool {
+        guard sourceAngle >= 0 && sourceAngle <= 360 else {
+            return false
         }
+        guard startAngle >= 0 && startAngle <= 360 else {
+            return false
+        }
+        guard degrees >= -360 && degrees <= 360 else {
+            return false
+        }
+        return true
     }
     
     fileprivate func degreesDifference(a: Float, b: Float, clockwise: Bool) -> Float {
@@ -139,6 +159,27 @@ class CircularAnimation {
         return CGPoint(x: CGFloat(x), y: CGFloat(y))
     }
     
+}
+
+extension CircularAnimation {
+    
+    enum Mode {
+        case enter
+        case exit
+    }
+    
+    enum AnimatingOrder {
+        case `default`
+        case reversed
+    }
+    
+    struct Options {
+        var animatingOrder: AnimatingOrder?
+        var duration: CFTimeInterval?
+        var delay: CFTimeInterval?
+        var timingFunction: CAMediaTimingFunction?
+    }
+    
     struct TimingFunction {
         // https://gist.github.com/naoyashiga/2673e55a9b5212fd0897
         // Visualization: https://easings.net/en
@@ -150,6 +191,15 @@ class CircularAnimation {
         }
         static func easeOutExpo() -> CAMediaTimingFunction {
             return CAMediaTimingFunction(controlPoints: 0.19, 1, 0.22, 1)
+        }
+        static func easeInQuint() -> CAMediaTimingFunction {
+            return CAMediaTimingFunction(controlPoints: 0.755, 0.05, 0.855, 0.06)
+        }
+        static func easeInQuart() -> CAMediaTimingFunction {
+            return CAMediaTimingFunction(controlPoints: 0.895, 0.03, 0.685, 0.22)
+        }
+        static func easeInExpo() -> CAMediaTimingFunction {
+            return CAMediaTimingFunction(controlPoints: 0.95, 0.05, 0.795, 0.035)
         }
     }
     
